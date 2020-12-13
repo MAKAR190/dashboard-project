0736a27187b2434d3af72df1e4cb04a30a64d820
unknown <lutskyimakar@gmail.com>
2020-12-13 12:06:13 +0200

    setup finished

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,27 @@
+// import { urls } from '../../routes';
+
+// import dashboardIcon from '../../images/dashboard-icon.svg';
+
+// const links = [
+//   {
+//     key: uuid(),
+//     label: 'Dashboard',
+//     to: urls.dashboard,
+//     icon: dashboardIcon,
+//   },
+// ];
+
+// const Navlink = () => null;
+
+function Sidebar() {
+  return (
+    <aside>
+      <h2>Sidebar</h2>
+      {/* {links.map((linkData) => (
+        <Navlink key={linkData.key} {...linkData} />
+      ))} */}
+    </aside>
+  );
+}
+
+export default Sidebar;
